1ef105f1082fc73a9080ca4fc077b7c23d546ddf
Techno573 <lucasmmtechno573@gmail.com>
2025-05-28 11:14:51 -0300

    Commit #2
    -Added Bicorn Hat
    -Added Fancy Bicorn Hat
    -Added Fancy Tricorn Hat
    -Added Bandana

--- src/main/java/net/techno573/scurvydogs/item/ModCreativeModeTabs.java
+++ src/main/java/net/techno573/scurvydogs/item/ModCreativeModeTabs.java
@@ -15,8 +15,12 @@ public class ModCreativeModeTabs {
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> SCURVY_DOGS_TAB = CREATIVE_MODE_TABS.register("scurvy_dogs_tab", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.scurvy_dogs")) //The language key for the title of your CreativeModeTab
             .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> ModItems.TRICORN_HAT.get().getDefaultInstance())
+            .icon(() -> ModItems.FANCY_TRICORN_HAT.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
                 output.accept(ModItems.TRICORN_HAT.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+                output.accept(ModItems.FANCY_TRICORN_HAT.get());
+                output.accept(ModItems.BICORN_HAT.get());
+                output.accept(ModItems.FANCY_BICORN_HAT.get());
+                output.accept(ModItems.BANDANA.get());
             }).build());
 }
--- src/main/java/net/techno573/scurvydogs/item/ModItems.java
+++ src/main/java/net/techno573/scurvydogs/item/ModItems.java
@@ -1,13 +1,11 @@
 package net.techno573.scurvydogs.item;
 
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.component.DyedItemColor;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -16,11 +14,44 @@ import net.techno573.scurvydogs.ScurvyDogsMod;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ScurvyDogsMod.MOD_ID);
 
+    //Tricorn Hat
     public static final DeferredItem<Item> TRICORN_HAT = ITEMS.registerSimpleItem("tricorn_hat", new Item.Properties()
             .equippable(EquipmentSlot.HEAD)
-            .durability(55)
+            .durability(60)
+            .enchantable(16)
             .attributes(ItemAttributeModifiers.builder().add(
                     Attributes.ARMOR,new AttributeModifier(ResourceLocation.withDefaultNamespace("armor"),1.0d,
-                            AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD).build())
-            .component(DataComponents.DYED_COLOR, new DyedItemColor(-6265536,false)));
+                            AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD).build()));
+    //Fancy Tricorn Hat
+    public static final DeferredItem<Item> FANCY_TRICORN_HAT = ITEMS.registerSimpleItem("fancy_tricorn_hat", new Item.Properties()
+            .equippable(EquipmentSlot.HEAD)
+            .durability(60)
+            .enchantable(16)
+            .attributes(ItemAttributeModifiers.builder().add(
+                    Attributes.ARMOR,new AttributeModifier(ResourceLocation.withDefaultNamespace("armor"),1.0d,
+                            AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD).build()));
+    //Bicorn Hat
+    public static final DeferredItem<Item> BICORN_HAT = ITEMS.registerSimpleItem("bicorn_hat", new Item.Properties()
+            .equippable(EquipmentSlot.HEAD)
+            .durability(60)
+            .enchantable(16)
+            .attributes(ItemAttributeModifiers.builder().add(
+                    Attributes.ARMOR,new AttributeModifier(ResourceLocation.withDefaultNamespace("armor"),1.0d,
+                            AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD).build()));
+    //Fancy Bicorn Hat
+    public static final DeferredItem<Item> FANCY_BICORN_HAT = ITEMS.registerSimpleItem("fancy_bicorn_hat", new Item.Properties()
+            .equippable(EquipmentSlot.HEAD)
+            .durability(60)
+            .enchantable(16)
+            .attributes(ItemAttributeModifiers.builder().add(
+                    Attributes.ARMOR,new AttributeModifier(ResourceLocation.withDefaultNamespace("armor"),1.0d,
+                            AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD).build()));
+    //Bandana
+    public static final DeferredItem<Item> BANDANA = ITEMS.registerSimpleItem("bandana", new Item.Properties()
+            .equippable(EquipmentSlot.HEAD)
+            .durability(60)
+            .enchantable(16)
+            .attributes(ItemAttributeModifiers.builder().add(
+                    Attributes.ARMOR,new AttributeModifier(ResourceLocation.withDefaultNamespace("armor"),1.0d,
+                            AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD).build()));
 }
